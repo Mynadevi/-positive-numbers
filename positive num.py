cda1e6863aaabe74e7c30d4ba4f922ae5af7e797
Mynadevi <75661805+Mynadevi@users.noreply.github.com>
2020-12-15 13:00:49 +0530

    positive num.py
    This file prints only the positive numbers in a list

---
+++ positive num.py
@@ -0,0 +1,9 @@
+#positive numbers in lists
+mylist[]
+n=int(input("Enter the number of elements"))
+for in range(0,n):
+      val=int(input())
+      mnylist.append(val)
+for num in mylist:
+      if num>=0:
+         print(num,end="")
