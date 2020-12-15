3c9f00d5a221cd288cfc417f594d07971d4d9140
Mynadevi <75661805+Mynadevi@users.noreply.github.com>
2020-12-15 13:06:11 +0530

    Update positive num.py

--- positive num.py
+++ positive num.py
@@ -3,7 +3,7 @@ mylist[]
 n=int(input("Enter the number of elements"))
 for in range(0,n):
       val=int(input())
-      mnylist.append(val)
+      mylist.append(val)
 for num in mylist:
       if num>=0:
          print(num,end="")
